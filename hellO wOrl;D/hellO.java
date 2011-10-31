41e47b83681b3ad7663d9c6033cca8d8e26dc06d
José Miguel Malaca <cartoonfull@gmail.com>
2011-10-31 02:15:25 +0000

    commit finaL, hOpe so

---
+++ hellO wOrl;D/hellO.java
@@ -0,0 +1,14 @@
+/*
+ * hello world em java
+ * @author JoséŽ Miguel Malaca
+ * https://github.com/tOOnPT
+ * 
+ */
+public class hellO {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hellO github.com wOrlD");
+	}
+
+}
